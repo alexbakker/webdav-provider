771fca2916aa71a1e85ccf2c98c65a811d7b892c
Alexander Bakker <ab@alexbakker.me>
2021-08-13 14:55:36 +0200

    Revert settings to defaults if an error occurs while loading

--- app/src/main/java/me/alexbakker/webdav/modules/ApplicationModule.kt
+++ app/src/main/java/me/alexbakker/webdav/modules/ApplicationModule.kt
@@ -1,12 +1,15 @@
 package me.alexbakker.webdav.modules
 
 import android.content.Context
+import android.widget.Toast
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
+import me.alexbakker.webdav.R
 import me.alexbakker.webdav.settings.Settings
+import java.io.IOException
 import javax.inject.Singleton
 
 @Module
@@ -16,10 +19,15 @@ class ApplicationModule {
     @Provides
     @Singleton
     fun provideSettings(@ApplicationContext context: Context): Settings {
-        return if (Settings.fileExists(context)) {
-            Settings.readFile(context)
-        } else {
-            Settings()
+        try {
+            if (Settings.fileExists(context)) {
+                return Settings.readFile(context)
+            }
+        } catch (e: IOException) {
+            e.printStackTrace()
+            Toast.makeText(context, R.string.error_loading_settings, Toast.LENGTH_LONG).show()
         }
+
+        return Settings()
     }
 }
