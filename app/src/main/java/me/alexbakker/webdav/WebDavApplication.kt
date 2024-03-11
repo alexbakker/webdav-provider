9268d91dabbc043154048d57b1c26bd89de85e2f
Alexander Bakker <ab@alexbakker.me>
2024-03-11 21:47:03 +0100

    Apply dynamic colors to the theme if available

--- app/src/main/java/me/alexbakker/webdav/WebDavApplication.kt
+++ app/src/main/java/me/alexbakker/webdav/WebDavApplication.kt
@@ -1,7 +1,13 @@
 package me.alexbakker.webdav
 
 import android.app.Application
+import com.google.android.material.color.DynamicColors
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
-class WebDavApplication : Application()
+class WebDavApplication : Application() {
+    override fun onCreate() {
+        super.onCreate()
+        DynamicColors.applyToActivitiesIfAvailable(this)
+    }
+}
