e1fe459b885265c3b265720bae67d99d062dd3b6
Alexander Bakker <ab@alexbakker.me>
2024-03-31 19:39:59 +0200

    Trigger a file rescan in Nextcloud after test initialization
    Nextcloud gets confused if we change the files without going through the
    WebDAV server

--- tests/controller/server.py
+++ tests/controller/server.py
@@ -12,4 +12,6 @@ def health():
 @app.post("/reset/<container>")
 def reset_container(container: str):
     subprocess.check_call(["/init.sh", f"/dav/{container}"], stdout=sys.stdout, stderr=sys.stderr)
+    if container == "nextcloud":
+        subprocess.check_call(["nc", "nextcloud", "1337"], stdout=sys.stdout, stderr=sys.stderr)
     return "OK"
--- tests/servers/nextcloud/run.sh
+++ tests/servers/nextcloud/run.sh
@@ -7,4 +7,7 @@ mkdir -p "${DATA_DIR}"
 chown -R www-data:www-data "${DATA_DIR}"
 chmod g+s "${DATA_DIR}"
 
+# Allow the controller to trigger a file rescan in the Nextcloud container
+su - www-data -s /bin/bash -c "while true; do nc -l -p 1337 -c 'cd /var/www/html && php occ files:scan --all'; done" &
+
 exec /entrypoint.sh apache2-foreground
